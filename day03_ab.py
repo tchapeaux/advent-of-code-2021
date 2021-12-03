c71572fe7d2d4808c0e56005f9d95d11f597292d
Thomas Chapeaux <tom@wooclap.com>
2021-12-03 10:07:46 +0100

    add disclaimer for future employers

--- day03_ab.py
+++ day03_ab.py
@@ -3,6 +3,9 @@ import aoc
 # data = aoc.getCellsInput(3, force_filepath="inputs/day3_example.txt")
 data = aoc.getCellsInput(3)
 
+# This code is ugly
+# Leaving it in all its messy glory because that is the AoC way
+
 print("\n".join(["".join(row) for row in data]))
 
 gamma = []
