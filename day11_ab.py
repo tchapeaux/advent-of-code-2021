dff69cd7218d7b265a5b932ee37dca8bcc07dec7
Thomas Chapeaux <tom@wooclap.com>
2021-12-11 12:39:45 +0100

    day 11 cleanup
    removed a useless "copy" variable which I thought would be useful but actually no

--- day11_ab.py
+++ day11_ab.py
@@ -18,38 +18,35 @@ part2Solution = None
 
 for step in range(NB_OF_STEPS):
     print("Step", step + 1)
-    # duplicate table
-    newData = [[x for x in row] for row in data]
 
-    # phase 1 - increase all by 1
+    # phase 1 - increase all cells by 1
     for y in range(len(data)):
         for x in range(len(data[y])):
-            newData[y][x] += 1
+            data[y][x] += 1
 
     # phase 2 - mark flashing cells
-    # this will be done in many iteration until all flashes are covered
+    # Because any flash can trigger new flashes,
+    # this will be done in multiple iteration until all flashes are covered
     flashing = set()
     flashIsGrowingFlag = True
     while flashIsGrowingFlag:
         flashIsGrowingFlag = False
         for y in range(len(data)):
             for x in range(len(data[y])):
-                if newData[y][x] > 9 and (x, y) not in flashing:
+                if data[y][x] > 9 and (x, y) not in flashing:
                     flashIsGrowingFlag = True
                     flashing.add((x, y))
                     flashAcc += 1
-                    for neighX, neighY, _ in aoc.get8Neighbors(newData, x, y):
-                        newData[neighY][neighX] += 1
+                    for neighX, neighY, _ in aoc.get8Neighbors(data, x, y):
+                        data[neighY][neighX] += 1
 
     print("flashed", len(flashing), "times")
 
     # phase 3 - power down flashing cells to 0
     for x, y in flashing:
-        newData[y][x] = 0
+        data[y][x] = 0
 
-    # Cleanup before next loop
-    data = newData
-
+    # Check if one of the solution was reached
     if step == 99:
         part1Solution = flashAcc
     if len(flashing) == NUMBER_OF_CELLS and not part2Solution:
